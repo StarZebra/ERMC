8fd1d461c3025d5b4eb101237a43bf5519b1361f
StarZebra <93840635+StarZebra@users.noreply.github.com>
2025-04-09 16:42:58 +0200

    infected arrows +5 seconds of load time (thanks Material.values()

--- src/main/java/me/starzebra/ermc/ArrowTrailListener.java
+++ src/main/java/me/starzebra/ermc/ArrowTrailListener.java
@@ -1,8 +1,8 @@
 package me.starzebra.ermc;
 
-import org.bukkit.Material;
-import org.bukkit.Particle;
-import org.bukkit.Sound;
+import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,15 +11,34 @@ import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitScheduler;
 import org.bukkit.util.Vector;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class ArrowTrailListener implements Listener {
+
+    long shootDelay = 250;
+    long lastShot = 0;
+
+    ArrayList<Location> infectedBlocks = new ArrayList<>();
+
+    BukkitScheduler scheduler = Main.getScheduler();
+    Plugin plugin = Main.getInstance();
 
     @EventHandler
     public void onRightClick(PlayerInteractEvent event){
         if(event.getAction() == Action.LEFT_CLICK_BLOCK || event.getAction() == Action.LEFT_CLICK_AIR || event.getItem() == null) return;
         if(event.getItem().getType() != Material.BOW) return;
+
         event.setCancelled(true);
+
+        long now = System.currentTimeMillis();
+        if(now - lastShot < shootDelay) return;
+        lastShot = now;
 
         Player player = event.getPlayer();
 
@@ -31,7 +50,8 @@ public class ArrowTrailListener implements Listener {
         player.playSound(player.getEyeLocation(), Sound.ENTITY_ARROW_SHOOT, 0.5f, 1.2f);
         for (Vector vec : shootVectors) {
             Arrow specialArrow = player.launchProjectile(Arrow.class, vec.multiply(2));
-            specialArrow.setMetadata("specialArrow", new FixedMetadataValue(Main.getInstance(), true));
+            specialArrow.setMetadata("specialArrow", new FixedMetadataValue(plugin, true));
+            specialArrow.setMetadata("infect_block", new FixedMetadataValue(plugin, true));
         }
 
     }
@@ -54,13 +74,13 @@ public class ArrowTrailListener implements Listener {
 
         Projectile arrow = event.getEntity();
 
-        Main.getScheduler().runTaskTimer(Main.getInstance(), (task) -> {
+        scheduler.runTaskTimer(plugin, (task) -> {
             if(!arrow.hasMetadata("inactive")){     //vv this bullshit for offsetX is the color of the note?? (n)/24 = color where 0<=n<=24
                 Particle.NOTE.builder().offset((arrow.getTicksLived() % 24)/24f, 0, 0).location(arrow.getLocation()).count(0).receivers(32).spawn();
                 return;
             }
             task.cancel();
-        }, 1L, 2L);
+        }, 2L, 2L);
 
     }
 
@@ -70,13 +90,48 @@ public class ArrowTrailListener implements Listener {
         if(!(event.getEntity().getShooter() instanceof Player)) return;
         Arrow arrow = (Arrow) event.getEntity();
         arrow.setPickupStatus(AbstractArrow.PickupStatus.DISALLOWED);
-        arrow.setMetadata("inactive", new FixedMetadataValue(Main.getInstance(), true));
+        arrow.setMetadata("inactive", new FixedMetadataValue(plugin, true));
         if(event.getHitEntity() != null && arrow.hasMetadata("specialArrow")){
             LivingEntity entity = (LivingEntity) event.getHitEntity();
             entity.setNoDamageTicks(2);
         }
 
-        Main.getScheduler().runTaskLater(Main.getInstance(), arrow::remove, 100L);
+        if(event.getHitBlock() != null && arrow.hasMetadata("infect_block")){
+            arrow.remove();
+
+            Block hitBlock = event.getHitBlock();
+            World world = hitBlock.getWorld();
+            Location loc = hitBlock.getLocation();
+            BlockData hitBlockData = hitBlock.getBlockData();
+
+            if(infectedBlocks.contains(loc)) return;
+            infectedBlocks.add(loc);
+
+            world.setBlockData(loc, Material.BONE_BLOCK.createBlockData());
+
+            scheduler.runTaskLater(plugin, () -> {
+                infectedBlocks.remove(loc);
+                world.setBlockData(loc, hitBlockData);
+            }, 100L);
+
+            scheduler.runTaskTimer(plugin, (task) -> {
+                if(infectedBlocks.contains(loc)){
+                    world.setBlockData(loc, getRandomBlockData());
+                }else {
+                    task.cancel();
+                }
+            }, 0L, 5L);
+        }
+
+
+
+    }
+
+    private BlockData getRandomBlockData(){
+        Random rand = new Random();
+        List<Material> mats = Main.getNonLegacyBlockMaterials();
+        Material randMat = mats.get(rand.nextInt(mats.size()));
+        return randMat.createBlockData();
     }
 
 }
--- src/main/java/me/starzebra/ermc/Main.java
+++ src/main/java/me/starzebra/ermc/Main.java
@@ -1,13 +1,20 @@
 package me.starzebra.ermc;
 
+import org.bukkit.Material;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 
 public final class Main extends JavaPlugin {
 
     private static BukkitScheduler scheduler;
     private static Main plugin;
+
+    private static List<Material> nonLegacyBlockMaterials;
 
     public static JavaPlugin getInstance(){
         return plugin;
@@ -17,18 +24,26 @@ public final class Main extends JavaPlugin {
         return scheduler;
     }
 
+    public static List<Material> getNonLegacyBlockMaterials(){
+        return nonLegacyBlockMaterials;
+    }
+
     @Override
     public void onEnable() {
         // Plugin startup logic i.e. register events and commands
         getLogger().info("ERMC plugin has been enabled!");
+        plugin = this;
+        scheduler = getServer().getScheduler();
 
         PluginManager pm = getServer().getPluginManager();
         //pm.registerEvents(new BlockBreakListener(), this);
         pm.registerEvents(new SnowballTurretListener(), this);
         pm.registerEvents(new BoneInteractListener(), this);
         pm.registerEvents(new ArrowTrailListener(), this);
-        plugin = this;
-        scheduler = getServer().getScheduler();
+
+        Predicate<Material> isNonLegacySolidBlock = (block) -> block.isBlock() && !block.isLegacy() && block.isSolid();
+        nonLegacyBlockMaterials = Arrays.stream(Material.values()).filter(isNonLegacySolidBlock).toList();
+
 
     }
 
